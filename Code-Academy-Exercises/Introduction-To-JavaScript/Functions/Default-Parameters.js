812d75637d5cdc37c1e8c58351d9f7adf4ee1517
snakedoc <ty.maxey@gmail.com>
2019-01-21 14:02:49 -0700

    Functions

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Default-Parameters.js
@@ -0,0 +1,7 @@
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
+
+/* Default parameters allow parameters to have a predetermined value in case there is no argument passed into the function or if the argument is undefined when called  */
