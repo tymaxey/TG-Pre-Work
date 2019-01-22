2baa39bfc2541b980f4d2d5cf30c226dbf509f9a
snakedoc <ty.maxey@gmail.com>
2019-01-21 20:29:33 -0700

    Compatibility

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/caniusedotcom-1.js
@@ -0,0 +1,3 @@
+
+
+//  it's important for you to know how to look up browser support on a feature-by-feature basis/ ES6 looks limited globally
---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/caniusedotcom-2.js
@@ -0,0 +1,5 @@
+// Set the variable below to a number
+let esFivePercentageSupport = 95.38;
+
+// Set the variable below to a number
+let esSixTemplateLiterals = 88.67;
